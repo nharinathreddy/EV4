ca942dcab38304ec3f9bf94e5c4bb7d8fe26f92b
nharinathreddy <60137667+nharinathreddy@users.noreply.github.com>
2020-03-22 20:31:42 +0530

    Update __init__.py

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -2,4 +2,4 @@ from .data_loader import *
 from .train_model import *
 from .test_model import *
 from .grad_cam import *
-from torch_lr_finder.lr_finder import LRFinder
+from .lr_finder import LRFinder
